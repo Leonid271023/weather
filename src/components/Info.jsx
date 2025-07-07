5c3e70742c2ab4410ea2e70c4ed0a75c70cb3008
leonid <lashurov44@gmail.com>
2025-07-07 18:11:28 +0300

    First Commit

---
+++ src/components/Info.jsx
@@ -0,0 +1,10 @@
+const Info = () => {
+    return (
+        <div>
+                <h1>weather application</h1>
+                <p>your city weather</p>
+        </div>
+    );
+};
+
+export default Info;
